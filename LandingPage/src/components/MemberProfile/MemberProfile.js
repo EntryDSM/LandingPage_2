5dbebdd98b85d77d0e1ef70156faf76c5274ddbb
hyejun <hyejun0608@naver.com>
2020-10-18 02:26:51 +0900

    컴포넌트 나눠서 다시 제작 반응형 X

---
+++ LandingPage/src/components/MemberProfile/MemberProfile.js
@@ -0,0 +1,153 @@
+import React from 'react'
+import style from 'styled-components'
+import Profiles from './Profiles'
+import { KJW, KJH, LWC, LYS, SWJ, Male, Female, JWY } from '../../img/Member'
+
+const Team = style.div `
+    font-size: 65px;
+    font-weight: bold;
+    font-family: 'NanumGothic', sans-serif;
+    line-height: 75px;
+    letter-spacing: 10px;
+    text-align: center;
+    margin-top: 105px;
+`;
+const Explain = style.div `
+    font-size: 20px;
+    font-family: 'NanumGothic', sans-serif;
+    line-height: 35px;
+    text-align: center;
+    margin-top: 20px;
+`;
+const Allprofiles = style.div `
+    max-width: 1070px;
+    min-height: 1120px;
+    margin: 0 auto;
+    margin-top: 200px;
+`;
+const Line = style.div `
+    max-width: 1068px;
+    height: 250px;
+    display: flex;
+    margin-bottom: 41px;
+`;
+const MemberProfile = () => {
+    return(
+        <div>
+            <Team>TEAM ENTRY</Team>
+            <Explain>
+                EntryDSM의 발전과 새로운 이야기를 만드는 사람들 입니다.
+            </Explain>
+            <Allprofiles>
+                <Line>
+                    <Profiles
+                        name = "김준우"
+                        grade = "third"
+                        img = { KJW }
+                        fb = 'FRONT - END'
+                        github = 'https://github.com/junu126'
+                    />
+                    <Profiles
+                        name = "김재훈"
+                        grade = "third"
+                        img = { KJH }
+                        fb = 'BACK - END, DEVOPS'
+                        github = 'https://github.com/LumpKim'
+                    />
+                    <Profiles
+                        name = "김해건"
+                        grade = "third"
+                        img = { Male }
+                        fb = 'UX/UI DESIGNER'
+                        github = ''
+                    />
+                    <Profiles
+                        name = "신은주"
+                        grade = "third"
+                        img = { SWJ }
+                        fb = 'BACK - END'
+                        github = 'https://github.com/Gaegul'
+                    />
+                </Line>
+                <Line>
+                <Profiles
+                        name = "장소희"
+                        grade = "third"
+                        img = { Female }
+                        fb = 'FRONT - END'
+                        github = 'https://github.com/soheejang15'
+                    />
+                    <Profiles
+                        name = "황신우"
+                        grade = "third"
+                        img = { Male }
+                        fb = 'BACK - END'
+                        github = 'https://github.com/huewilliams'
+                    />
+                    <Profiles
+                        name = "김대웅"
+                        grade = "second"
+                        img = { Male }
+                        fb = 'BACK - END'
+                        github = 'https://github.com/dkssud9556'
+                    />
+                    <Profiles
+                        name = "오준상"
+                        grade = "second"
+                        img = { Male }
+                        fb = 'FRONT - END'
+                        github = 'https://github.com/sema0710'
+                    />
+                </Line>
+                <Line>
+                    <Profiles
+                        name = "이우찬"
+                        grade = "second"
+                        img = { LWC }
+                        fb = 'FRONT - END'
+                        github = 'https://github.com/woochanleee'
+                    />
+                    <Profiles
+                        name = "임용성"
+                        grade = "second"
+                        img = { LYS }
+                        fb = 'BACK - END'
+                        github = 'https://github.com/LeagueLugas'
+                    />
+                    <Profiles
+                        name = "정우영"
+                        grade = "second"
+                        img = { JWY }
+                        fb = 'PM, BACK - END'
+                        github = 'https://github.com/o-ozogie'
+                    />
+                    <Profiles
+                        name = "김혜준"
+                        grade = "first"
+                        img = { Female }
+                        fb = 'FRONT - END'
+                        github = 'https://github.com/hyejun0608'
+                    />
+                </Line>
+                <Line>
+                    <Profiles
+                        name = "임서영"
+                        grade = "first"
+                        img = { Female }
+                        fb = 'DESIGNER'
+                        github = ''
+                    />
+                    <Profiles
+                        name = "전규현"
+                        grade = "first"
+                        img = { Male }
+                        fb = 'FRONT - END'
+                        github = 'https://github.com/Ghyeon1215'
+                    />
+                </Line>
+            </Allprofiles>
+        </div>
+    )
+}
+
+export default MemberProfile;
