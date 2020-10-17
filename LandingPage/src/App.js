9a74fdab6cc7ce0e965a94a29daec6bff8ace044
hyejun <hyejun0608@naver.com>
2020-10-17 22:21:50 +0900

    Landing Page

---
+++ LandingPage/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AboutEntry from './components/AboutEntry';
+import Footer from './components/Footer';
+import MemberProfile from './components/MemberProfile';
+import EntryQnA from './components/EntryQnA'
+
+function App() {
+  return (
+    <div>
+      <AboutEntry />
+      <MemberProfile />
+      <EntryQnA />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
