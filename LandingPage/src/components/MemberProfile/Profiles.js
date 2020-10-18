92856b4eb476f5cb698fc99edf0f66e291682b8b
ohjunsang <pandati@dsm.hs.kr>
2020-10-18 22:59:05 +0900

    [FIX] profile hover 시에 text 이상하게 뜨는 버그 수정

--- LandingPage/src/components/MemberProfile/Profiles.js
+++ LandingPage/src/components/MemberProfile/Profiles.js
@@ -1,56 +1,65 @@
-import React from 'react'
-import style from 'styled-components'
+import React from "react";
+import style from "styled-components";
 
-const Profile = style.div `
+const Profile = style.div`
     margin-right: 3.6vw;
 `;
-const Img = style.div `
+const Img = style.div`
     position: relative;
     width: 209px;
     height: 209px;
     border-radius: 105px;
     & > img {
         border-radius: 50%;
-        margin: 8px
-    }
-    & > img:hover {
-        opacity: 0.4;
-        + div {
-            opacity: 1;
-        }
-        + div:hover {
-            opacity: 1;
-        }
+        margin: 8px;
+        position: absolute;
+        top: 0;
+        left: 0;
     }
 
 `;
-const Grade = style(Img) `
-    border: ${props => props.grade === 'first' ? '1px solid #0015D5' : props.grade === 'second' ? '1px solid #707070' : '1px solid #FFEE00'};
+const Grade = style(Img)`
+    border: ${(props) =>
+      props.grade === "first"
+        ? "1px solid #0015D5"
+        : props.grade === "second"
+        ? "1px solid #707070"
+        : "1px solid #FFEE00"};
+    &:hover > img {
+        opacity: 0.4;
+    }
+    &:hover > div {
+        opacity: 1;
+    }
     & > div {
         opacity: 0;
+        width: 209px;
+        height: 209px;
+        z-index: 3;
+        position: absolute;
+        text-align: center;
+        top: 70px;
     }
 `;
-const FB = style.div `
-    position: absolute;
+const FB = style.div`
     width: 100%;
-    margin-top: 7px;
     text-align: center;
     top: 70px;
     font-family: 'NanumGothic', sans-serif;
     font-size: 18px;
     font-weight: bold;
+    margin-bottom: 30px;
 `;
-const Github = style.button `
-    position: absolute;
+const Github = style.button`
     bottom: 70px;
     left: 70px;
     font-size: 16px;
     font-family: 'NanumGothic', sans-serif;
     font-weight: bold;
-    background-color: white;
     border: none;
+    background-color: rgba(255, 255, 255, 0);
 `;
-const Name = style.div `
+const Name = style.div`
     font-size: 25px;
     font-weight: bold;
     font-family: 'NanumGothic', sans-serif;
@@ -58,28 +67,37 @@ const Name = style.div `
     margin-top: 7px;
 `;
 
-const Profiles = props => {
-    const { name, grade, img, fb, github} = props;
-    return (
-        <div>
-            <Profile>
-                <Grade grade={ grade }>
-                    <img src={ img }
-                        width='194'
-                        height='194'
-                        border-radius='100'
-                        alt='profile' />
-                    <div>        
-                        <FB>{ fb }</FB>
-                        <Github>
-                            <a href={ github } target='_blank' rel='noopener noreferrer' style={{textDecoration: 'none', color: 'black'}}>Github</a>
-                        </Github>
-                    </div>
-                </Grade>
-                <Name>{ name }</Name>
-            </Profile>
-        </div>
-    );
-}
+const Profiles = (props) => {
+  const { name, grade, img, fb, github } = props;
+  return (
+    <div>
+      <Profile>
+        <Grade grade={grade}>
+          <img
+            src={img}
+            width="194"
+            height="194"
+            border-radius="100"
+            alt="profile"
+          />
+          <div>
+            <FB>{fb}</FB>
+            <Github>
+              <a
+                href={github}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Github
+              </a>
+            </Github>
+          </div>
+        </Grade>
+        <Name>{name}</Name>
+      </Profile>
+    </div>
+  );
+};
 
 export default Profiles;
