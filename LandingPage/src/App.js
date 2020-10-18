903e62fa0d554dc00c1293646630797bc5e47b27
hyejun <hyejun0608@naver.com>
2020-10-18 18:01:41 +0900

    member 컴포넌트 경로 수정

--- LandingPage/src/App.js
+++ LandingPage/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import AboutEntry from './components/AboutEntry';
 import Footer from './components/Footer';
-import MemberProfile from './components/MemberProfile';
+import MemberProfile from './components/MemberProfile/MemberProfile';
 import EntryQnA from './components/EntryQnA'
-
 function App() {
   return (
     <div>
