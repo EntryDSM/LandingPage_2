47e24f8e5552dec07be23eef98a8d2ae00622082
hyejun <hyejun0608@naver.com>
2020-10-18 21:45:24 +0900

    두 작업 합침

--- LandingPage/src/App.js
+++ LandingPage/src/App.js
@@ -3,9 +3,20 @@ import AboutEntry from './components/AboutEntry';
 import Footer from './components/Footer';
 import MemberProfile from './components/MemberProfile/MemberProfile';
 import EntryQnA from './components/EntryQnA'
+import {GlobalStyle} from './GlobalStyle';
+import Header from './components/Header';
+import MainBtn from './components/MainBtn';
+import ServiceEntry from './components/ServiceEntry';
+import Portfolio from './components/Portfolio';
+
 function App() {
   return (
     <div>
+      <GlobalStyle />
+      <Header />
+      <MainBtn />
+      <ServiceEntry />
+      <Portfolio />
       <AboutEntry />
       <MemberProfile />
       <EntryQnA />
