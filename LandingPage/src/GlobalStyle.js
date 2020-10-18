a8cefe5ccc883f23a0d0cf20918636b8fc06279b
hyejun <hyejun0608@naver.com>
2020-10-18 21:49:45 +0900

    GlobalStyle

---
+++ LandingPage/src/GlobalStyle.js
@@ -0,0 +1,17 @@
+import styled, { createGlobalStyle } from 'styled-components';
+export const GlobalStyle = createGlobalStyle`
+    body {
+        padding: 0;
+        margin: 0;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    @font-face {
+        src: url('/fontSs/BBTreeG_B.ttf') format('truetype');
+      }
+`;
